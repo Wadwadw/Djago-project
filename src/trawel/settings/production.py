2cec5d86dc85bb28c9bcb6a0196e45492f2f6ac9
Господин <sumsunh777@gmail.com>
2021-06-29 18:10:09 +0300

    git

--- src/trawel/settings/production.py
+++ src/trawel/settings/production.py
@@ -28,7 +28,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0dcg-893klziix_lb_fg2tg8vh)+k%)yc-_^9s9+i676n+9$4d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'trawel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
