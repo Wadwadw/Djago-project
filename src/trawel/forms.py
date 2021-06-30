b24c9b75c6ac0bdf311a94d4bfaca01a0278a90a
Господин <sumsunh777@gmail.com>
2021-06-30 15:20:04 +0300

    git

--- src/trawel/forms.py
+++ src/trawel/forms.py
@@ -2,15 +2,13 @@ from django import forms
 from django.contrib.auth import authenticate
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
+    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}), required=False)
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
-
         if username and password:
-            user = authenticate(user_name=username, password=password)
+            user = authenticate(username=username, password=password)
             if not user:
                 raise forms.ValidationError('User not find')
             if not user.check_password(password):
